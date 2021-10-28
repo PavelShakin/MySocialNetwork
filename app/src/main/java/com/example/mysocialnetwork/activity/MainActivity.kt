0f05750185ed3bc5bb1130dd7d72a9e60f35d824
Pavel Shakin <pashkin302@gmail.com>
2021-10-28 15:05:34 +0300

    Add setOnClickListener method and database usage

--- app/src/main/java/com/example/mysocialnetwork/activity/MainActivity.kt
+++ app/src/main/java/com/example/mysocialnetwork/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mysocialnetwork.activity
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -9,19 +10,20 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.mysocialnetwork.R
-import com.example.mysocialnetwork.view_model.UserViewModel
+import com.example.mysocialnetwork.view_model.UsersListViewModel
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewModel: UserViewModel
+    private lateinit var viewModel: UsersListViewModel
 
     @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
-        viewModel.loadUserData()
+        viewModel = ViewModelProvider(this).get(UsersListViewModel::class.java)
+        viewModel.fillUpDatabase()
+        viewModel.loadUsersData()
 
-        viewModel.userLiveData.observe(this, Observer { it ->
+        viewModel.userLiveData.observe(this, {
             for (id in 0..8) {
                 val layout = findViewById<ConstraintLayout>(
                     resources.getIdentifier("user$id", "id", packageName)
@@ -30,12 +32,12 @@ class MainActivity : AppCompatActivity() {
                 val txtViewName = findViewById<TextView>(
                     resources.getIdentifier("usrName$id", "id", packageName)
                 )
-                txtViewName.text = it.usersList[id].name
+                txtViewName.text = it[id].name
 
                 val txtViewTime = findViewById<TextView>(
                     resources.getIdentifier("usrTime$id", "id", packageName)
                 )
-                txtViewTime.text = it.usersList[id].wasOnline
+                txtViewTime.text = it[id].wasOnline
 
                 val imgViewIcon = findViewById<ImageView>(
                     resources.getIdentifier("profileIcon$id", "id", packageName)
@@ -43,9 +45,10 @@ class MainActivity : AppCompatActivity() {
                 imgViewIcon.setImageDrawable(
                     getDrawable(
                         resources.getIdentifier(
-                            it.usersList[id].profilePhoto,
+                            it[id].profilePhoto,
                             null,
-                            packageName)
+                            packageName
+                        )
                     )
                 )
 
@@ -55,6 +58,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setOnClickListener(layout: ConstraintLayout, id: Int) {
-        viewModel.setListener(layout, id, this)
+        layout.setOnClickListener {
+            val intent = Intent(this, UserActivity::class.java)
+            intent.putExtra("id", id)
+            startActivity(intent)
+        }
     }
 }
