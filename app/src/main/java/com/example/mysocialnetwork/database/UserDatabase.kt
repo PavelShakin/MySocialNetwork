24011b23b5e9150f645259c83ebf224ec659ad76
Pavel Shakin <pashkin302@gmail.com>
2021-10-28 15:19:58 +0300

    Created database

---
+++ app/src/main/java/com/example/mysocialnetwork/database/UserDatabase.kt
@@ -0,0 +1,38 @@
+package com.example.mysocialnetwork.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.mysocialnetwork.user.User
+
+@Database(entities = [User::class], version = 1)
+abstract class UserDatabase : RoomDatabase() {
+
+    abstract fun userDatabaseDao(): UserDatabaseDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: UserDatabase? = null
+
+        fun getInstance(context: Context): UserDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        UserDatabase::class.java,
+                        "users_info_table"
+                    )
+                        .allowMainThreadQueries()
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
