e4da59890c412ec523d0535f3b7fc9929b95fea3
Pavel Shakin <pashkin302@gmail.com>
2021-10-28 15:11:00 +0300

    Created UsersList viewModel

---
+++ app/src/main/java/com/example/mysocialnetwork/view_model/UsersListViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.mysocialnetwork.view_model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.mysocialnetwork.database.UserDatabase
+import com.example.mysocialnetwork.user.User
+import com.example.mysocialnetwork.user.UsersData
+
+class UsersListViewModel(application: Application) : AndroidViewModel(application) {
+    private var usersData: UsersData = UsersData()
+
+    private val _userLiveData = MutableLiveData<List<User>>()
+    val userLiveData: LiveData<List<User>> = _userLiveData
+
+    private val database = UserDatabase.getInstance(application).userDatabaseDao()
+
+    fun fillUpDatabase() {
+        if (database.isEmpty() == null) {
+            for (user in usersData.usersList) {
+                database.insert(user)
+            }
+        }
+    }
+
+    fun loadUsersData() {
+        _userLiveData.value = database.getAll()
+    }
+}
