199342d4f0f58b04f172179bfc03710d4cdeb5d0
Pavel Shakin <pashkin302@gmail.com>
2021-10-28 15:09:00 +0300

    Simplified list of users creation

--- app/src/main/java/com/example/mysocialnetwork/user/UsersData.kt
+++ app/src/main/java/com/example/mysocialnetwork/user/UsersData.kt
@@ -13,62 +13,42 @@ class UsersData {
         const val phPath9 = "@drawable/ic_user9"
     }
 
-    var usersList: ArrayList<User> = ArrayList()
-
-    init {
-        usersList.add(
-            User(
-                "Jon", phPath1, "was online 1 hour ago", "Reading",
-                "bkslvr1987@gmail.com", "Enjoy reading"
-            )
+    var usersList = listOf(
+        User(
+            0, "Jon", phPath1, "was online 1 hour ago", "Reading",
+            "bkslvr1987@gmail.com", "Enjoy reading"
+        ),
+        User(
+            1, "Bob", phPath2, "was online 12 minutes ago", "Swimming",
+            "bbbbbob2133@gmail.com", "100m: 23 sec"
+        ),
+        User(
+            2, "Ann", phPath3, "was online 10 hour ago", "Football",
+            "goldnsdf@gmail.com", "Training for Gold Ball"
+        ),
+        User(
+            3, "Ferdinand", phPath4, "was online 3 days ago", "Golf",
+            "aaaaaaa@gmail.com", "qwerty"
+        ),
+        User(
+            4, "David", phPath5, "was online yesterday ago", "Climbing",
+            "b2342555@gmail.com", "Hills is my life"
+        ),
+        User(
+            5, "Paul", phPath6, "online", "Ride a bike",
+            "biker2001@gmail.com", "Broke leg yesterday"
+        ),
+        User(
+            6, "Mike", phPath7, "was online a week ago", "PC games",
+            "fdsfa@gmail.com", "Silent Hill lover"
+        ),
+        User(
+            7, "Julia", phPath8, "was online 6 years ago", "Cooking",
+            "qwerty@gmail.com", "You know how to boil a water?"
+        ),
+        User(
+            8, "Kelly", phPath9, "was online 3 month ago", "Sleep",
+            "zzzzzz@gmail.com", "Sleep, eat, repeat"
         )
-        usersList.add(
-            User(
-                "Bob", phPath2, "was online 12 minutes ago", "Swimming",
-                "bbbbbob2133@gmail.com", "100m: 23 sec"
-            )
-        )
-        usersList.add(
-            User(
-                "Ann", phPath3, "was online 10 hour ago", "Football",
-                "goldnsdf@gmail.com", "Training for Gold Ball"
-            )
-        )
-        usersList.add(
-            User(
-                "Ferdinand", phPath4, "was online 3 days ago", "Golf",
-                "aaaaaaa@gmail.com", "qwerty"
-            )
-        )
-        usersList.add(
-            User(
-                "David", phPath5, "was online yesterday ago", "Climbing",
-                "b2342555@gmail.com", "Hills is my life"
-            )
-        )
-        usersList.add(
-            User(
-                "Paul", phPath6, "online", "Ride a bike",
-                "biker2001@gmail.com", "Broke leg yesterday"
-            )
-        )
-        usersList.add(
-            User(
-                "Mike", phPath7, "was online a week ago", "PC games",
-                "fdsfa@gmail.com", "Silent Hill lover"
-            )
-        )
-        usersList.add(
-            User(
-                "Julia", phPath8, "was online 6 years ago", "Cooking",
-                "qwerty@gmail.com", "You know how to boil a water?"
-            )
-        )
-        usersList.add(
-            User(
-                "Kelly", phPath9, "was online 3 month ago", "Sleep",
-                "zzzzzz@gmail.com", "Sleep, eat, repeat"
-            )
-        )
-    }
+    )
 }
