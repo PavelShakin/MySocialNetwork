ca4025798946b0fbc2cd6bc85569960a232bb885
Pavel Shakin <pashkin302@gmail.com>
2021-10-28 15:08:02 +0300

    Added column, changed to data class

--- app/src/main/java/com/example/mysocialnetwork/user/User.kt
+++ app/src/main/java/com/example/mysocialnetwork/user/User.kt
@@ -1,11 +1,30 @@
 package com.example.mysocialnetwork.user
 
-class User(
-    val name: String,
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users_info_table")
+data class User(
+
+    @PrimaryKey
+    val userId: Int,
+
+    @ColumnInfo(name = "users_full_name")
+    var name: String,
+
+    @ColumnInfo(name = "users_profile_photo")
     val profilePhoto: String,
+
+    @ColumnInfo(name = "user_was_online_time")
     val wasOnline: String,
-    val hobby: String,
-    val email: String,
-    val status: String
-) {
-}
+
+    @ColumnInfo(name = "users_hobby")
+    var hobby: String,
+
+    @ColumnInfo(name = "users_email")
+    var email: String,
+
+    @ColumnInfo(name = "users_status")
+    var status: String
+)
