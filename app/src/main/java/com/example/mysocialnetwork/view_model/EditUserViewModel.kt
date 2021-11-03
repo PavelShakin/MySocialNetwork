d70248a63fdc0e904cdd9b7135e149489dfe1346
Pavel Shakin <pashkin302@gmail.com>
2021-11-03 18:27:04 +0200

    Created viewModel for EditUserActivity

---
+++ app/src/main/java/com/example/mysocialnetwork/view_model/EditUserViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.mysocialnetwork.view_model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.mysocialnetwork.database.UserDatabase
+import com.example.mysocialnetwork.user.User
+
+class EditUserViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _userLiveData = MutableLiveData<User>()
+    val userLiveData: LiveData<User> = _userLiveData
+
+    private val database = UserDatabase.getInstance(application).userDatabaseDao
+
+    fun loadUserData(id: Int) {
+        _userLiveData.value = database.get(id)
+    }
+
+    fun updateUserInfo(user: User) {
+        database.update(user)
+    }
+}
